20c8c332d9558c91c2339f735873604dbb80985f
al-ell <>
2023-09-27 18:25:28 +0000

    feat: API functional

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,18 @@
 // API key from https://developer.themoviedb.org/docs
 
 // home page end point:
-const apiUrl = 'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=2fcc9b1d9695840cb0066635e69b01b2&page=1/';
+const apiUrl = 'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=S&sort_by=popularity.desc&api_key=2fcc9b1d9695840cb0066635e69b01b2&page=1/';
 // Movie poster file path:
 const imgPath = 'https://image.tmdb.org/t/p/w1280';
 // Search bar endpoint:
-const searchUrl = 'https://api.themoviedb.org/3/search/movie?certification_country=US&certification.lte=G&api_key=2fcc9b1d9695840cb0066635e69b01b2&page=1&query="/';
+const searchAPI = 'https://api.themoviedb.org/3/search/movie?certification_country=US&certification.lte=S&sort_by=popularity.desc&api_key=2fcc9b1d9695840cb0066635e69b01b2&page=1/';
 
+// const apiUrl = 'https://api.themoviedb.org/3/discover/movie?certification_country=U&certification=G&certification.gte=G&certification.lte=GS&include_adult=false&language=en-US&region=US&sort_by=popularity.desc&with_genres=%10751%&api_key=2fcc9b1d9695840cb0066635e69b01b2&page=1&query="/';
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search-input');
+const formBtn = document.getElementById('submit-btn');
 
 // get initial movies
 getMovies(apiUrl);
@@ -16,6 +22,59 @@ getMovies(apiUrl);
 async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.results);
+
+    showMovies(data.results);
 }
 
+function showMovies(movies) {
+    MediaDeviceInfo.innerHTML = '';
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+        <img src="${imgPath + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3 class="movie-title">${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview</h3>
+            <p>${overview}</p>
+        </div>`;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    };
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== '') {
+        getMovies(searchAPI + searchTerm);
+
+        search.value = '';
+    } else {
+        window.location.reload();
+    }
+});
+
+
+// Not in tutorial but I will add click function to search bar 
+//formBtn.addEventListener('click', );
