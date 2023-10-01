fbed44ed981b9f3392287f63563460ab5d460c17
al-ell <>
2023-10-01 07:25:20 +0000

    feat: homepage returning family films

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,16 @@
 // API key from https://developer.themoviedb.org/docs
 
 // home page end point:
-const apiUrl = 'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=S&sort_by=popularity.desc&api_key=2fcc9b1d9695840cb0066635e69b01b2&page=1/';
+// API url update to return aminated films: https://www.themoviedb.org/talk/60114a2cdd83fa003f99bcfb 
+const apiUrl = 'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&with_genres=10751&include_adult=false&sort_by=popularity.desc&api_key=2fcc9b1d9695840cb0066635e69b01b2&page=1/';
 // Movie poster file path:
 const imgPath = 'https://image.tmdb.org/t/p/w1280';
 // Search bar endpoint:
-const searchAPI = 'https://api.themoviedb.org/3/search/movie?certification_country=US&certification.lte=S&sort_by=popularity.desc&api_key=2fcc9b1d9695840cb0066635e69b01b2&page=1/';
-
-// const apiUrl = 'https://api.themoviedb.org/3/discover/movie?certification_country=U&certification=G&certification.gte=G&certification.lte=GS&include_adult=false&language=en-US&region=US&sort_by=popularity.desc&with_genres=%10751%&api_key=2fcc9b1d9695840cb0066635e69b01b2&page=1&query="/';
+const searchAPI = 'https://api.themoviedb.org/3/search/movie?certification_country=US&certification.lte=G&with_genres=10751&include_adult=false&sort_by=popularity.desc&api_key=2fcc9b1d9695840cb0066635e69b01b2&query="';
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
-const search = document.getElementById('search-input');
+const search = document.getElementById('search');
 const formBtn = document.getElementById('submit-btn');
 
 // get initial movies
@@ -27,7 +26,7 @@ async function getMovies(url) {
 }
 
 function showMovies(movies) {
-    MediaDeviceInfo.innerHTML = '';
+    main.innerHTML = '';
 
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie;
